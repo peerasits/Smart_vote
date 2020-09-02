0099ace9e8116a47611feff98db30862ba35f518
Peerasit Suntieakchun <peerasit11@gmail.com>
2020-09-02 11:48:25 +0700

    W2_first committed

--- app/src/main/java/th/ac/su/cp/smartvote/MainActivity.java
+++ app/src/main/java/th/ac/su/cp/smartvote/MainActivity.java
@@ -2,7 +2,10 @@ package th.ac.su.cp.smartvote;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button nextButton = findViewById(R.id.next_button);
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent checkIdIntent = new Intent(MainActivity.this,Check_id.class);
+                startActivity(checkIdIntent);
+            }
+        });
+
     }
 }
